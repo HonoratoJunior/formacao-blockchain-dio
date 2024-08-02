8687c599a8b9830d2512a91b2d1f9a311bcacba1
Honorato Junior <kallelhonorato@hotmail.com>
2024-08-02 16:32:33 -0300

    Primeiro Token

--- Modulo 03 Desenvolvimento com Solidity/Curso 02 Desenvolvimento de Smart Contracts/Criando a sua primeira criptomoeda da Rede Ethereum/MeuToken.sol
+++ Modulo 03 Desenvolvimento com Solidity/Curso 02 Desenvolvimento de Smart Contracts/Criando a sua primeira criptomoeda da Rede Ethereum/MeuToken.sol
@@ -3,12 +3,10 @@ pragma solidity ^0.8.0;
 
 interface IERC20{
 
-    //getters
     function totalSupply() external view returns(uint256);
     function balanceOf(address account) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
 
-    //functions
     function transfer(address recipient, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
